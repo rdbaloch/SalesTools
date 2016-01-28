63ecb672bda776f0779348badd727a5da3049be0
Rashed <davodi@hotmail.com>
2016-01-28 14:43:13 +0100

    Initial Commit for salesTools

---
+++ LocalSalesTools/SalesTools/src/ca/demo/salestools/Main.java
@@ -0,0 +1,26 @@
+package ca.demo.salestools;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		
+		displayGretting();
+		data.display();
+		
+		
+		
+	}
+	
+	private static void displayGretting(){
+		
+			System.out.println("Hello happy salles people");
+			System.out.println("This app shows  sales data");
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LocalSalesTools/SalesTools/src/ca/demo/salestools/SalesData.java
@@ -0,0 +1,12 @@
+	package ca.demo.salestools;
+
+public class SalesData {
+	
+	int data [] = {0, 4, 42};
+	
+	
+	public void display(){
+		System.out.println("TODO: implement data dispay");
+	}
+
+}
